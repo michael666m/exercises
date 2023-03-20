164f09af05ebf4b1614799e4526b8af9b4a3fcff
Michael Meese <michael.meese@jungheinrich.de>
2023-03-20 11:15:23 +0000

    V1.0

---
+++ cloud-cap/hello/srv/world.js
@@ -0,0 +1,10 @@
+module.exports = class say {
+  hello(req) {
+    let {to} = req.data
+    if (to === 'me') 
+    {
+      to = require('os').userInfo().username
+    }
+    return `Hello ${to}!`
+  }
+}
